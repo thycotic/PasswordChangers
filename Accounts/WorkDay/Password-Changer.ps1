78156136be394d91a47122941e3046eb8a37e16a
Ali <ali@afalahi.com>
2017-11-25 15:45:17 -0500

    Create Password-Changer.ps1
    this is a WIP

---
+++ Accounts/WorkDay/Password-Changer.ps1
@@ -0,0 +1,19 @@
+$loginUrl = "https://wd5.myworkday.com/wday/authgwy/masonicvillages/login.htmld"
+#initiate internet explorer object
+$ie = New-Object -ComObject "internetexplorer.application"
+$ie.visible = $true
+$ie.navigate2($loginUrl)
+#wait untill IE is ready
+while ($ie.Busy -eq $true) { Start-Sleep -Seconds 2; }
+$document=$ie.Document
+#click on the change password link
+($document.getElementsByTagName("a") | where {$_.innerText -eq "Change Password"}).click();
+#get the last form fields that are relevant to password changing
+$fields= $document.getElementsByTagName("input") | Select -Last 4
+#pass the values to the form fields
+$fields[0].value="ali@afalahi.com" #username
+$fields[1].value="password@1" #old password
+$fields[2].value="password@2" #new password
+$fields[3].value="password@2" #verify password
+#submit the password change
+($document.getElementsByTagName("button") | where {$_.innerText -eq "Submit"}).click();
