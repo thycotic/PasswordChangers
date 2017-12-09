e9923170d141dd9a755aa50f410b19609cf26486
Ali <ali@afalahi.com>
2017-12-09 09:01:34 -0500

    Update AWS-Root-Heartbeat.ps1

--- Accounts/AWS-Root/AWS-Root-Heartbeat.ps1
+++ Accounts/AWS-Root/AWS-Root-Heartbeat.ps1
@@ -16,8 +16,8 @@ catch
         throw "Error: " + $Error[0].Exception.Message
     }
 
-$email="afalahi@live.com"
-$password="mo%/J2xCL*}TMGj3"
+$email=""
+$password=""
 $loginUrl ="https://signin.aws.amazon.com/signin?client_id=arn%3Aaws%3Aiam%3A%3A015428540659%3Auser%2Fhomepage&redirect_uri=https%3A%2F%2Fconsole.aws.amazon.com%2Fconsole%2Fhome%3Fstate%3DhashArgs%2523%26isauthcode%3Dtrue&page=resolve"
 #initiate internet explorer object
 $ie = New-Object -ComObject "internetexplorer.application"
