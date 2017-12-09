e40ea74e1bfa318426e3d79007070fc5549d16dc
Ali <ali@afalahi.com>
2017-12-09 10:19:19 -0500

    Update AWS-Root-Heartbeat.ps1

--- Accounts/AWS-Root/AWS-Root-Heartbeat.ps1
+++ Accounts/AWS-Root/AWS-Root-Heartbeat.ps1
@@ -21,34 +21,40 @@ $password=""
 $loginUrl ="https://signin.aws.amazon.com/signin?client_id=arn%3Aaws%3Aiam%3A%3A015428540659%3Auser%2Fhomepage&redirect_uri=https%3A%2F%2Fconsole.aws.amazon.com%2Fconsole%2Fhome%3Fstate%3DhashArgs%2523%26isauthcode%3Dtrue&page=resolve"
 #initiate internet explorer object
 $ie = New-Object -ComObject "internetexplorer.application"
-$ie.visible = $false
+$ie.visible = $true
 $ie.navigate2($loginUrl)
-$document=$ie.Document
-while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }
-if($ie.LocationURL -like "*awsemail*")
-{
-    $document=$ie.Document
-    ($document.GetElementById("aws-login-switchaccount-link")).click();
-}
-while ($ie.Busy -eq $true) { Start-Sleep -Seconds 2; }
-$document=$ie.Document
-#get the first  form fields that are relevant to sign in
-$document.GetElementById("resolving_input").value=$email
-#pass the values to the form fields
-($document.getElementsByTagName("button") | where {$_.innerText -eq "Next"}).click();
-Start-Sleep -Seconds 5;
-#enter password
-($document.GetElementById("ap_password")| select -First 1).value=$password;
-#Sign In
-($document.GetElementById("signInSubmit-input")).click();
-while ($ie.Busy -eq $true) { Start-Sleep -Seconds 2; }
-if($ie.LocationName -eq "AWS Management Console"){
-($document.GetElementById("aws-console-logout")).click();
-while ($ie.Busy -eq $true) { Start-Sleep -Seconds 2; }
-$ie.Quit();
-return $true
-}
-else{
-$ie.Quit();
-throw "Error heartbeating account" + $Error[0].Exception.Message
-}
+try
+    {
+        while ($ie.Busy -eq $true) { Start-Sleep -Seconds 2; }
+        $document=$ie.Document
+        #get the first  form fields that are relevant to sign in
+        $document.GetElementById("resolving_input").value=$email
+        #pass the values to the form fields
+        ($document.getElementsByTagName("button") | where {$_.innerText -eq "Next"}).click();
+        Start-Sleep -Seconds 5;
+        #enter password
+        ($document.GetElementById("ap_password")| select -First 1).value=$password;
+        #Sign In
+        ($document.GetElementById("signInSubmit-input")).click();
+        while ($ie.Busy -eq $true) { Start-Sleep -Seconds 2; }
+        if($ie.LocationName -ne "AWS Management Console")
+            {
+                throw "Error heartbeating account"
+            }
+        else
+            {
+                ($document.GetElementById("aws-console-logout")).click();
+                return $true
+            }
+    }
+catch
+    {
+
+        throw 'Error "{0}"' -f $_
+
+    }
+finally
+    {
+        while ($ie.Busy -eq $true) { Start-Sleep -Seconds 2; }
+        $ie.Quit();
+    }
