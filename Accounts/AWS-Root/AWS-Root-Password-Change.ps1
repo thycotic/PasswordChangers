24750b5e15af0ff4539ef6a7502f2cba201e084b
Ali <ali@afalahi.com>
2017-12-10 09:21:26 -0500

    Update AWS-Root-Password-Change.ps1

--- Accounts/AWS-Root/AWS-Root-Password-Change.ps1
+++ Accounts/AWS-Root/AWS-Root-Password-Change.ps1
@@ -1,10 +1,27 @@
-$email=""
-$password=""
-$newPassword=""
-$loginUrl = "https://signin.aws.amazon.com/signin?client_id=arn%3Aaws%3Aiam%3A%3A015428540659%3Auser%2Fhomepage&redirect_uri=https%3A%2F%2Fconsole.aws.amazon.com%2Fconsole%2Fhome%3Fstate%3DhashArgs%2523%26isauthcode%3Dtrue&page=resolve"
+$dllPath="C:\Program Files (x86)\Microsoft.NET\Primary Interop Assemblies\Microsoft.mshtml.dll"
+try
+    {
+        $checkDll=Get-Item -Path $dllPath -ErrorAction SilentlyContinue
+        if($checkDll.Exists)
+            {
+                Add-Type -Path $dllPath
+            }
+        else
+            {
+                throw "Could not load Microsoft.mshtml.dll please make sure that it exist in C:\Program Files (x86)\Microsoft.NET\Primary Interop Assemblies\"
+            }
+     }
+catch
+    {
+        throw "Error: " + $Error[0].Exception.Message
+    }
+$email=$args[0]
+$password=$args[1]
+$newPassword=$args[2]
+$loginUrl = $args[3]
 #initiate internet explorer object
 $ie = New-Object -ComObject "internetexplorer.application"
-$ie.visible = $true
+$ie.visible = $false
 $ie.navigate($loginUrl)
 try
     {
@@ -13,7 +30,7 @@ try
         #Enter Username
         $document.GetElementById("resolving_input").value=$email
         ($document.getElementsByTagName("button") | where {$_.innerText -eq "Next"}).click();
-        Start-Sleep 5
+        Start-Sleep 3
         #Enter Password
         ($document.GetElementById("ap_password")| select -First 1).value=$password;
         #Sign In
@@ -21,7 +38,7 @@ try
         while ($ie.Busy -eq $true) { Start-Sleep -Seconds 2; }
         #navigate to password change page
         ($document.GetElementById("aws-security-credentials")).click()
-        Start-Sleep 5
+        Start-Sleep 3
         ($document.getElementsByTagName("a") | where {$_.textContent -eq "Click here"}).click();
         while ($ie.Busy -eq $true) { Start-Sleep -Seconds 2; }
         ($document.getElementById("cnep_1A_change_password_button-input")).click();
